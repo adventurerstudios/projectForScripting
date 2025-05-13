e5e9a15e00cd94aae0e84b6b6f7b6337a547d7a1
josie533 <josie.sturtz@gmail.com>
2025-05-12 22:43:42 -0500

    main menu updated, need to know your plans to know what to do next add user done. scraper working

--- Final/Main_Menu.py
+++ Final/Main_Menu.py
@@ -1,14 +1,15 @@
-from user_data import add_user
+from user_data import *
 from StealthScrape import scraper
 def menu ():
     while True:
         print("\nChoose one of the following: "
               "\n1. Create an account"
               "\n2. Log in"
-              "\n3. Check for leaks"
-              "\n4. Import/Export data"
-              "\n5. Scrape"
-              "\n6. Quit")
+              "\n3. View users"
+              "\n4. Check for leaks"
+              "\n5. Import/Export data"
+              "\n6. Scrape"
+              "\n7. Quit")
         choice = input("What would you like to do?: ")
 
         if choice == "1":
@@ -22,10 +23,13 @@ def menu ():
         elif choice == "4":
 
         elif choice == "5":
-            scraper(input("Enter website: "))
+            get_all_users()
         elif choice == "6":
+            scraper(input("Enter website: "))
+        elif choice == "7":
             print("Goodbye")
             return
         else:
             print("Invalid choice, try again.")
+            return
 menu()
